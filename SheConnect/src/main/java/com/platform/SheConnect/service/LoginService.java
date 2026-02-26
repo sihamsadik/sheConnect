9e7e3f25f08a646799ff580025dfedfe95c0d46d
sihamsadik <mssgntt@gmail.com>
2026-02-26 13:28:38 +0300

    fix: add dto for login

--- SheConnect/src/main/java/com/platform/SheConnect/service/LoginService.java
+++ SheConnect/src/main/java/com/platform/SheConnect/service/LoginService.java
@@ -2,13 +2,10 @@ package com.platform.SheConnect.service;
 
 import com.platform.SheConnect.entity.User;
 import com.platform.SheConnect.service.UserService;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;  
-import org.springframework.security.core.credentials.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+@Service
 
 public class LoginService {
 
@@ -29,7 +26,7 @@ public class LoginService {
         }
 
 
-        return new LoginResponse(user.getName(), user.getEmail(), user.getRole());
+        return new LoginResponse(user.getName(), user.getEmail(), user.getRole().getName());
     }
 
 }
