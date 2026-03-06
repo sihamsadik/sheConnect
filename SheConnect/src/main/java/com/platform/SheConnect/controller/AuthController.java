dcf5580bb3394eb87ae6de3f93a398e6c6936c07
sihamsadik <mssgntt@gmail.com>
2026-03-06 16:05:29 +0300

    fix : auth controller  for refresh token

--- SheConnect/src/main/java/com/platform/SheConnect/controller/AuthController.java
+++ SheConnect/src/main/java/com/platform/SheConnect/controller/AuthController.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 // import org.apache.catalina.User;
 import org.springframework.http.ResponseEntity;
 import com.platform.SheConnect.security.RefreshTokenService;
+import com.platform.SheConnect.security.JwtService;
 
 // import com.platform.SheConnect.service.RegisterService;
 import com.platform.SheConnect.service.AuthService;
 
 import com.platform.SheConnect.dto.LoginRequest;
 import com.platform.SheConnect.dto.LoginResponse;
+import com.platform.SheConnect.dto.RefreshResponse;
 import com.platform.SheConnect.dto.RegisterRequest;
 import com.platform.SheConnect.entity.User;
 import com.platform.SheConnect.entity.RefreshToken;
@@ -25,13 +27,15 @@ public class AuthController {
     // private final RegisterService registerService;
     private final AuthService authService;
     private final RefreshTokenService refreshTokenService;
+    private final JwtService jwtService;
 
     // private final LoginService loginService;
 
-    public AuthController(AuthService authService, RefreshTokenService refreshTokenService) {
+    public AuthController(AuthService authService, RefreshTokenService refreshTokenService, JwtService jwtService) {
         // this.registerService = registerService;
         this.authService = authService;
         this.refreshTokenService = refreshTokenService;
+        this.jwtService = jwtService;
     }
     @PostMapping("/register")
     public ResponseEntity<LoginResponse> register(@RequestBody RegisterRequest request) {
@@ -50,14 +54,20 @@ public class AuthController {
         return ResponseEntity.ok("Logged out successfully");
     }
     @PostMapping("/refresh")
-    public ResponseEntity<RefreshToken> refresh(@RequestBody String refreshToken) {
+    public ResponseEntity<RefreshResponse> refresh(@RequestBody String refreshToken) {
 
         User user = refreshTokenService.validateRefreshToken(refreshToken).getUser();
          RefreshToken response = refreshTokenService.validateRefreshToken(refreshToken);
         if (response == null) {
             return ResponseEntity.status(401).body(null);
-        }  
-        response = refreshTokenService.createRefreshToken(user);
-        return ResponseEntity.ok(response);
+        }
+        String newAccessToken = jwtService.generateAccessToken(user.getEmail(), user.getRole().getName());
+        String newRefreshToken = refreshTokenService.createRefreshToken(user).getToken();
+
+        RefreshResponse refreshResponse = new RefreshResponse(newAccessToken, newRefreshToken);
+        return ResponseEntity.ok(refreshResponse);
     }
-}
+
+    }
+        
+    
