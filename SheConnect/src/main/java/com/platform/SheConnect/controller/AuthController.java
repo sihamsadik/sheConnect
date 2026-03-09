3d546f4ed64b907bd7afd1cc67c04de940590041
sihamsadik <mssgntt@gmail.com>
2026-03-09 09:41:07 +0300

    fix : auth controller  for refresh token

--- SheConnect/src/main/java/com/platform/SheConnect/controller/AuthController.java
+++ SheConnect/src/main/java/com/platform/SheConnect/controller/AuthController.java
@@ -10,11 +10,14 @@ import com.platform.SheConnect.security.RefreshTokenService;
 import com.platform.SheConnect.security.JwtService;
 
 // import com.platform.SheConnect.service.RegisterService;
+import com.platform.SheConnect.dto.RefreshTokenRequest;
 import com.platform.SheConnect.service.AuthService;
 
 import com.platform.SheConnect.dto.LoginRequest;
 import com.platform.SheConnect.dto.LoginResponse;
 import com.platform.SheConnect.dto.RefreshResponse;
+
+import com.platform.SheConnect.dto.RefreshTokenRequest;
 import com.platform.SheConnect.dto.RegisterRequest;
 import com.platform.SheConnect.entity.User;
 import com.platform.SheConnect.entity.RefreshToken;
@@ -49,22 +52,26 @@ public class AuthController {
         return ResponseEntity.ok(response);
     }
     @PostMapping("/logout")
-    public ResponseEntity<String> logout(@RequestBody String refreshToken) {
-        refreshTokenService.deleteRefreshToken(refreshToken);
+    public ResponseEntity<String> logout(@RequestBody RefreshTokenRequest refreshToken) {
+        refreshTokenService.deleteRefreshToken(refreshToken.getRefreshToken());
         return ResponseEntity.ok("Logged out successfully");
     }
     @PostMapping("/refresh")
-    public ResponseEntity<RefreshResponse> refresh(@RequestBody String refreshToken) {
+    public ResponseEntity<RefreshResponse> refresh(@RequestBody RefreshTokenRequest refreshTokenRequest) {
 
-        User user = refreshTokenService.validateRefreshToken(refreshToken).getUser();
-         RefreshToken response = refreshTokenService.validateRefreshToken(refreshToken);
+        User user = refreshTokenService.validateRefreshToken(refreshTokenRequest.getRefreshToken()).getUser();
+        System.out.println("user email: " + user.getEmail());
+         RefreshToken response = refreshTokenService.validateRefreshToken(refreshTokenRequest.getRefreshToken());
         if (response == null) {
+            System.out.println("Invalid refresh token");
             return ResponseEntity.status(401).body(null);
         }
+        refreshTokenService.deleteRefreshToken(refreshTokenRequest.getRefreshToken());
         String newAccessToken = jwtService.generateAccessToken(user.getEmail(), user.getRole().getName());
         String newRefreshToken = refreshTokenService.createRefreshToken(user).getToken();
 
         RefreshResponse refreshResponse = new RefreshResponse(newAccessToken, newRefreshToken);
+        System.out.println("New Access Token: " + newAccessToken);
         return ResponseEntity.ok(refreshResponse);
     }
 
